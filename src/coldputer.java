fa6dcc83a4083247f46f986bde4ea0ac69ea753e
trubb <effinew@gmail.com>
2018-09-10 16:52:23 +0200

    solved cold-puter science

--- src/coldputer.java
+++ src/coldputer.java
@@ -3,22 +3,25 @@ import java.util.Scanner;
 public class coldputer {
 
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        int nofinput = scanner.nextInt();
-
-        int[] temps = new int[nofinput];
-
-        for (int i = 0; i <= nofinput; i++) {
-
-            temps[i] = scanner.nextInt();
+        // initialize the counter
+        int cc = 0;
+        // initialize the scanner
+        Scanner in = new Scanner(System.in);
+        // inputs takes first input to determine how many temps there will be
+        int vals = in.nextInt();
+        /*
+            check each new input to determine if it is negative
+            until the proper number of inputs have been reached
+            then continue
+         */
+        for (int i = 0; i < vals; i++) {
+            if (in.nextInt() < 0) {
+                cc++;
+            }
         }
-
-        for (int i = 0; i < nofinput; i++) {
-
-            System.out.println(temps[i]);
-
-        }
-
+        // close the scanner
+        in.close();
+        // print the number of negative numbers
+        System.out.println(cc);
     }
 }
