9acd304ba9e66451b0ee7af1f50566275e36c6e9
erpette <erpette@kth.se>
2018-09-11 17:03:25 +0200

    solved carrots

---
+++ src/carrots.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class carrots {
+
+    public static void main(String[] args) {
+
+        int n, p;
+        Scanner in = new Scanner(System.in);
+
+        n = in.nextInt();
+        p = in.nextInt();
+
+        in.close();
+        System.out.println(p);
+
+    }
+
+}
