d4ab52bc887bc281e2e35934488ca8ec1e42f125
erpette <erpette@kth.se>
2018-09-11 17:03:07 +0200

    solved hissing mic

--- src/hissingmicrophone.java
+++ src/hissingmicrophone.java
@@ -5,6 +5,13 @@ public class hissingmicrophone {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
+        String s = in.next();
+
+        if (s.contains("ss")) {
+            System.out.println("hiss");
+        } else {
+            System.out.println("no hiss");
+        }
 
     }
 
