ef50b3dec90f87285d80e4a93f2118170dfcf175
erpette <erpette@kth.se>
2018-09-11 16:34:06 +0200

    solved r2

--- src/r2.java
+++ src/r2.java
@@ -5,6 +5,12 @@ public class r2 {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
+        int r1 = in.nextInt();
+        int s = in.nextInt();
 
+        int r2 = (s * 2) - r1;
+
+        System.out.println(r2);
     }
+
 }
