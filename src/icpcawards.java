417761c755f428e62ef4482d2e16eac4a7f2e2d1
erpette <erpette@kth.se>
2018-09-24 16:21:47 +0200

    adding icpcawards to be able to work at home

---
+++ src/icpcawards.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class icpcawards {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int nteams = in.nextInt();
+
+        String[] univs;
+        String[] teams;
+
+
+        // add university and team name to array
+        // when printing, check if university is in set
+        // add university to set if not present, if present, dont print
+
+        for (int i = 0; i < nteams; i++) {
+
+            String[] temp = in.nextLine().split("\\s+");
+
+
+
+
+        }
+
+    }
+
+}
