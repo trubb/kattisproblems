4baaa53dc9bc0bb60c0f09a5a49363aba65fbc39
trubb <effinew@gmail.com>
2018-09-24 21:59:55 +0200

    Solved icpcawards in a really really really horrible way string.split("\\s+") did not work as expected, the workaround worked but  index [1] was created but not adressable???

--- src/icpcawards.java
+++ src/icpcawards.java
@@ -1,27 +1,32 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class icpcawards {
 
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-
         int nteams = in.nextInt();
+        Set<String> words = new HashSet<>();
 
-        String[] univs;
-        String[] teams;
+        for (int i = 0; i <= nteams; i++) {
 
+            String tempstr = in.nextLine();
+            String[] temp = tempstr.split("\\s+");
 
-        // add university and team name to array
-        // when printing, check if university is in set
-        // add university to set if not present, if present, dont print
+/*            for (int j = 0; j < temp.length; j++) {
+                System.out.println(temp[j] + " index: " + j); // completely fine
+            }
+*/
+//            System.out.println(temp[0]); // fine
+//            System.out.println(temp[1]); // error
+//            System.out.println(tempstr + " " + temp[0] + " " + temp[1]); // error
 
-        for (int i = 0; i < nteams; i++) {
-
-            String[] temp = in.nextLine().split("\\s+");
-
-
-
+            if (!words.contains(temp[0]) && words.size() <= 12) {
+                words.add(temp[0]);
+                System.out.println(tempstr);
+            }
 
         }
 
