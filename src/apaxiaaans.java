1ea607bb734f18f01d3f3cb5044bd3bc12550aec
erpette <erpette@kth.se>
2018-09-24 15:18:43 +0200

    solved apaxiaaans

--- src/apaxiaaans.java
+++ src/apaxiaaans.java
@@ -5,8 +5,21 @@ public class apaxiaaans {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        String str = in.next();
+        char[] name = in.nextLine().toCharArray();
+        char prev = name[0];
 
+        System.out.print(prev); // print the first letter because we want it no matter what
+
+        for (int i = 0; i < name.length; i++) {
+
+            if (name[i] == prev) {
+                // do nothing
+            } else {
+                System.out.print(name[i]);
+            }
+
+            prev = name[i];
+        }
 
     }
 
