9eeb8a60532dcb7deb604257e039dcc62c3d2811
erpette <erpette@kth.se>
2018-09-25 13:11:09 +0200

    improved icpcawards with the help of loffa and sakjur

--- src/icpcawards.java
+++ src/icpcawards.java
@@ -7,25 +7,15 @@ public class icpcawards {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        int nteams = in.nextInt();
+        in.nextLine();
         Set<String> words = new HashSet<>();
 
-        for (int i = 0; i <= nteams; i++) {
+        while (words.size() < 12) {
+            String uniteam = in.nextLine();
+            String university = uniteam.split("\\s+")[0];
 
-            String tempstr = in.nextLine();
-            String[] temp = tempstr.split("\\s+");
-
-/*            for (int j = 0; j < temp.length; j++) {
-                System.out.println(temp[j] + " index: " + j); // completely fine
-            }
-*/
-//            System.out.println(temp[0]); // fine
-//            System.out.println(temp[1]); // error
-//            System.out.println(tempstr + " " + temp[0] + " " + temp[1]); // error
-
-            if (!words.contains(temp[0]) && words.size() <= 12) {
-                words.add(temp[0]);
-                System.out.println(tempstr);
+            if (words.add(university)) {
+                System.out.println(uniteam);
             }
 
         }
