eee222ac6b380c2b5d41600125fd387d65b9bb0c
erpette <erpette@kth.se>
2018-09-28 12:46:35 +0200

    moving pet to the cloud

---
+++ src/pet.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class pet {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        // ta in data somehow
+
+        // sätt index på spelarna 1-5
+
+        // räkna ut score
+
+        // räkna ut vinnare
+
+        // skriv ut vinnare
+
+        class competitor {
+
+            public int score (int[] arr) {
+
+                int sum;
+                for (int i = 0; i < arr.length; i++) {
+                    sum += arr[i];
+                }
+
+                return sum;
+            }
+
+
+        }
+
+    }
+
+}
