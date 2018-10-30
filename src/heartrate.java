c270ce8a9ccc7291af564419cc18700a7c0cbae0
erpette <erpette@kth.se>
2018-10-30 19:49:17 +0100

    solved heartrate

--- src/heartrate.java
+++ src/heartrate.java
@@ -5,7 +5,17 @@ public class heartrate {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
+        double b, p, diff, ebpm, low, high;
+        int cases = in.nextInt();
 
+        for (int i = 0; i < cases; i++) {
+            b = in.nextDouble();
+            p = in.nextDouble();
+            diff = 60.0 / p;
+            ebpm = diff * b;
+            low = ebpm - diff;
+            high = ebpm + diff;
+            System.out.format("%.4f " + "%.4f " + "%.4f" + "\n", low, ebpm, high);
+        }
     }
-
 }
